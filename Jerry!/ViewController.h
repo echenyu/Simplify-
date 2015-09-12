1f6e1327bd26501671ace675b4815292bf0e35a4
Eric Yu <echenyu@umich.edu>
2015-09-11 23:24:04 -0400

    Initial Commit

---
+++ Jerry!/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jerry!
+//
+//  Created by Eric Yu on 9/11/15.
+//  Copyright (c) 2015 AAEAA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Jerry!/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Jerry!
+//
+//  Created by Eric Yu on 9/11/15.
+//  Copyright (c) 2015 AAEAA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
